54865b0ec26a9dfca1fdb9d5479c170275e9c874
Erik Dunteman <erikdunteman@gmail.com>
2019-09-03 09:55:05 -0700

    Walking functionality (sine) tuned for on-board use of robot. Robot officially walking.

--- Ferdinand_Servo_Control/Ferdinand_Servo_Control.ino
+++ Ferdinand_Servo_Control/Ferdinand_Servo_Control.ino
@@ -1,21 +1,46 @@
 #include <Servo.h>
+
 Servo Right;
 Servo Left; 
 Servo Hips; 
 String inByte;
 int pos;
 int positions[3];
+int pinRA = 3;  // Connected to CLK on KY-040
+int pinRB = 4;  // Connected to DT on KY-040
+int encoderPosCount = 0; 
+int pinALast;  
+int aVal;
+
+void update_reward(){
+   aVal = digitalRead(pinA);
+   if (aVal != pinALast){ // Means the knob is rotating
+     // if the knob is rotating, we need to determine direction
+     // We do that by reading pin B.
+     if (digitalRead(pinB) != aVal) {  // Means pin A Changed first - We're Rotating Clockwise
+       encoderPosCount ++;
+     } else {// Otherwise B changed first and we're moving CCW
+       encoderPosCount--;
+     }
+   } 
+   pinALast = aVal;
+}
+  
 
 void setup() {
   Right.attach(9);
   Left.attach(8);
   Hips.attach(7);
+  pinMode (pinA,INPUT);
+  pinMode (pinB,INPUT);
+  pinALast = digitalRead(pinA);   
   Serial.begin(9600);
 }
 
 void loop()
-{    
-  if(Serial.available())  // if data available in serial port
+{
+  update_reward() ;  
+  if (Serial.available())  // if data available in serial port
   {
     int i = 0;
     while (i<3) {
@@ -27,10 +52,13 @@ void loop()
     Right.write(positions[0]);     // move servo
     Left.write(positions[1]);     // move servo
     Hips.write(positions[2]);     // move servo
-    Serial.print("Servo in positions: ");  
-    for (int i=0; i<3; i++){
-      Serial.println(positions[i]);
-    }
-
+//    Serial.print("Servo in positions: ");  
+//    for (int i=0; i<3; i++){
+//      Serial.println(positions[i]);
+//    }
+    String outstring = String(encoderPosCount);
+    outstring += " ";
+    outstring += String(encoderPosCount);
+    Serial.println(outstring);
     }
 }
--- Ferdinand_Servo_Control_Sine/Ferdinand_Servo_Control_Sine.ino
+++ Ferdinand_Servo_Control_Sine/Ferdinand_Servo_Control_Sine.ino
@@ -10,17 +10,17 @@ int hips_pos;
 int right_pos;
 int left_pos;
 int positions[3];
-int hips_home = 100;
-int right_home = 85;
-int left_home = 100;
-int range_val = 35;
+int hips_home = 140;
+int right_home = 65;
+int left_home = 130;
+int range_val = 25;
 float timer = 0;
 float pi = 3.14159;
 String dir;
 
 void get_pos(int hips_home, int right_home, int left_home, float pi, int range_val, float timer, int positions[3]){
-    hips_pos = hips_home + 0.8*range_val*sin(timer);
-    right_pos =  right_home + range_val*sin(timer - (pi/2)); // delay the period of the legs, as they are supposed to be half swing at bottom
+    hips_pos = hips_home + range_val*sin(timer);
+    right_pos =  right_home + range_val*sin(timer + (3*pi/2)); // delay the period of the legs, as they are supposed to be half swing at bottom
     left_pos =  left_home + range_val*sin(timer - (pi/2));
     positions[0] = hips_pos;
     positions[1] = right_pos;
@@ -30,8 +30,8 @@ void get_pos(int hips_home, int right_home, int left_home, float pi, int range_v
 
 void setup() {
   RightLeg.attach(9);  // attaches the servo on pin 9 to the servo object
-  LeftLeg.attach(8);
-  Hips.attach(7);   
+  LeftLeg.attach(10);
+  Hips.attach(11);   
   dir = "Up";
   Serial.begin(9600);
 }
@@ -46,7 +46,7 @@ void loop() {
 //  left_pos =  left_home + range_val*sin(timer - (pi/2));
   timer += pi/100;
     
-  Serial.println(hips_pos);
+  Serial.println(left_pos);
   RightLeg.write(right_pos);                  // sets the servo position according to the scaled value
   LeftLeg.write(left_pos);
   Hips.write(hips_pos);
